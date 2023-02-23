8a2396e1ab0768ca00717b7ea9200255b57a5ebe
dom <dominic@findsolutiongroup.com>
2023-02-23 15:09:34 +0800

    [LWW-004] refractoring test syntax structure

--- src/__tests__/LWWElementDictionary.test.ts
+++ src/__tests__/LWWElementDictionary.test.ts
@@ -4,131 +4,140 @@ import { Element } from '../utils/types';
 describe('LWW Element Dictionary Testing', () => {
   describe('Test Add & Lookup Function', () => {
     describe('Normal Case', () => {
-      it('Case(1): add one test data, test data should be inside addSet', () => {
-        // Arrange
-        const dict = new LWWElementDictionary();
-        const testKey = 'key1';
-        const testData: Element = {
-          string_data: 'a',
-          number_data: 123,
-          timestamp: Date.now(),
-        };
+      describe('Case(1): add one test data', () => {
+        it('test data should be inside addSet', () => {
+          // Arrange
+          const dict = new LWWElementDictionary();
+          const testKey = 'key1';
+          const testData: Element = {
+            string_data: 'a',
+            number_data: 123,
+            timestamp: Date.now(),
+          };
 
-        // Act
-        dict.add(testKey, testData);
+          // Act
+          dict.add(testKey, testData);
 
-        // Assert
-        expect(dict.getAddSet().get(testKey)).toEqual(testData);
-        expect(dict.lookup(testKey)).toBeTruthy();
+          // Assert
+          expect(dict.getAddSet().get(testKey)).toEqual(testData);
+          expect(dict.lookup(testKey)).toBeTruthy();
+        });
       });
-      it('Case(2): add one or more test data, test data should be inside addSet', () => {
-        //Arragne
-        const dict = new LWWElementDictionary();
-        const testKeys = ['key1', 'key2', 'key3'];
-        const testDatas: Element[] = [
-          {
-            string: 'a',
-            timestamp: 1,
-          },
-          {
-            string: 'b',
-            timestamp: 2,
-          },
-          {
-            string: 'c',
-            timestamp: 3,
-          },
-        ];
+      describe('Case(2): add one or more test data, test data should be inside addSet', () => {
+        it('test data should be inside addSet', () => {
+          //Arragne
+          const dict = new LWWElementDictionary();
+          const testKeys = ['key1', 'key2', 'key3'];
+          const testDatas: Element[] = [
+            {
+              string: 'a',
+              timestamp: 1,
+            },
+            {
+              string: 'b',
+              timestamp: 2,
+            },
+            {
+              string: 'c',
+              timestamp: 3,
+            },
+          ];
 
-        //Act
-        for (let index = 0; index < testKeys.length; index++) {
-          const key = testKeys[index];
-          const element = testDatas[index];
-          dict.add(key, element);
-        }
+          //Act
+          for (let index = 0; index < testKeys.length; index++) {
+            const key = testKeys[index];
+            const element = testDatas[index];
+            dict.add(key, element);
+          }
 
-        //Assert
+          //Assert
 
-        for (let index = 0; index < testKeys.length; index++) {
-          expect(dict.getAddSet().get(testKeys[index])).toEqual(
-            testDatas[index],
-          );
-          expect(dict.lookup(testKeys[index])).toBeTruthy();
-        }
+          for (let index = 0; index < testKeys.length; index++) {
+            expect(dict.getAddSet().get(testKeys[index])).toEqual(
+              testDatas[index],
+            );
+            expect(dict.lookup(testKeys[index])).toBeTruthy();
+          }
+        });
       });
-      it('Case(3) - LWW: add multiple test data with same keys, should only latest one stay', () => {
-        //Arrange
-        const dict = new LWWElementDictionary();
-        const testKey = 'key1';
-        const testDatas: Element[] = [
-          {
-            string: 'a',
-            timestamp: 1,
-          },
-          {
+
+      describe('Case(3) - LWW: add multiple test data with same keys, ', () => {
+        it('should only latest one stay', () => {
+          //Arrange
+          const dict = new LWWElementDictionary();
+          const testKey = 'key1';
+          const testDatas: Element[] = [
+            {
+              string: 'a',
+              timestamp: 1,
+            },
+            {
+              string: 'd',
+              timestamp: 4,
+            },
+            {
+              string: 'b',
+              timestamp: 2,
+            },
+            {
+              string: 'c',
+              timestamp: 3,
+            },
+          ];
+
+          //Act
+          for (let index = 0; index < testDatas.length; index++) {
+            const element = testDatas[index];
+            dict.add(testKey, element);
+          }
+
+          //Assert
+          expect(dict.getAddSet().get(testKey)).toBeDefined;
+          expect(dict.getAddSet().size).toBe(1);
+          expect(dict.getAddSet().get(testKey)).toEqual({
             string: 'd',
             timestamp: 4,
-          },
-          {
-            string: 'b',
-            timestamp: 2,
-          },
-          {
-            string: 'c',
-            timestamp: 3,
-          },
-        ];
-
-        //Act
-        for (let index = 0; index < testDatas.length; index++) {
-          const element = testDatas[index];
-          dict.add(testKey, element);
-        }
-
-        //Assert
-        expect(dict.getAddSet().get(testKey)).toBeDefined;
-        expect(dict.getAddSet().size).toBe(1);
-        expect(dict.getAddSet().get(testKey)).toEqual({
-          string: 'd',
-          timestamp: 4,
-        } as Element);
-        expect(dict.lookup(testKey)).toBeTruthy();
+          } as Element);
+          expect(dict.lookup(testKey)).toBeTruthy();
+        });
       });
-      it('Case(4) - LWW - 2: add multiple test data with same keys and diff keys, should only latest same key stay and diff key should also stay', () => {
-        //Arragne
-        const dict = new LWWElementDictionary();
-        const testKeys = ['key1', 'key2', 'key1'];
-        const testDatas: Element[] = [
-          {
-            string: 'a',
-            timestamp: 1,
-          },
-          {
-            string: 'b',
-            timestamp: 2,
-          },
-          {
+      describe('Case(4) - LWW - 2: add multiple test data with same keys and diff keys, ', () => {
+        it('should only latest same key stay and diff key should also stay', () => {
+          //Arragne
+          const dict = new LWWElementDictionary();
+          const testKeys = ['key1', 'key2', 'key1'];
+          const testDatas: Element[] = [
+            {
+              string: 'a',
+              timestamp: 1,
+            },
+            {
+              string: 'b',
+              timestamp: 2,
+            },
+            {
+              string: 'c',
+              timestamp: 3,
+            },
+          ];
+
+          //Act
+          for (let index = 0; index < testKeys.length; index++) {
+            const key = testKeys[index];
+            const element = testDatas[index];
+            dict.add(key, element);
+          }
+
+          //Assert
+          expect(dict.getAddSet().get('key1')).toBeDefined();
+          expect(dict.getAddSet().get('key2')).toBeDefined();
+          expect(dict.getAddSet().get('key1')).toEqual({
             string: 'c',
             timestamp: 3,
-          },
-        ];
-
-        //Act
-        for (let index = 0; index < testKeys.length; index++) {
-          const key = testKeys[index];
-          const element = testDatas[index];
-          dict.add(key, element);
-        }
-
-        //Assert
-        expect(dict.getAddSet().get('key1')).toBeDefined();
-        expect(dict.getAddSet().get('key2')).toBeDefined();
-        expect(dict.getAddSet().get('key1')).toEqual({
-          string: 'c',
-          timestamp: 3,
-        } as Element);
-        expect(dict.lookup('key1')).toBeTruthy();
-        expect(dict.lookup('key2')).toBeTruthy();
+          } as Element);
+          expect(dict.lookup('key1')).toBeTruthy();
+          expect(dict.lookup('key2')).toBeTruthy();
+        });
       });
     });
   });
@@ -186,92 +195,100 @@ describe('LWW Element Dictionary Testing', () => {
   });
   describe('Test Remove & Lookup  Function', () => {
     describe('Normal Case', () => {
-      it('Case(1), add one data to addSet (T=1) and then add to removeSet(T=2)', () => {
-        //Arrange
-        const dict = new LWWElementDictionary();
-        const testKey = 'key1';
-        const testData: Element = {
-          string_data: 'a',
-          timestamp: 1,
-        };
+      describe('Case(1), add one data to addSet (T=1) and then add to removeSet(T=2)', () => {
+        it('Lookup should return False', () => {
+          //Arrange
+          const dict = new LWWElementDictionary();
+          const testKey = 'key1';
+          const testData: Element = {
+            string_data: 'a',
+            timestamp: 1,
+          };
 
-        //Act
-        dict.add(testKey, testData);
-        dict.remove(testKey, 2);
+          //Act
+          dict.add(testKey, testData);
+          dict.remove(testKey, 2);
 
-        //Assert
-        expect(dict.getRemoveSet().size).toBe(1);
-        expect(dict.getAddSet().get(testKey)).toEqual({
-          string_data: 'a',
-          timestamp: 1,
-        } as Element);
-        expect(dict.getRemoveSet().get(testKey)).toEqual({
-          string_data: 'a',
-          timestamp: 2,
-        } as Element);
-        expect(dict.lookup(testKey)).toBeFalsy();
+          //Assert
+          expect(dict.getRemoveSet().size).toBe(1);
+          expect(dict.getAddSet().get(testKey)).toEqual({
+            string_data: 'a',
+            timestamp: 1,
+          } as Element);
+          expect(dict.getRemoveSet().get(testKey)).toEqual({
+            string_data: 'a',
+            timestamp: 2,
+          } as Element);
+          expect(dict.lookup(testKey)).toBeFalsy();
+        });
       });
-      it('Case(2), add one data to AddSet, and remove one data with non-exist key which should not affect other key in AddSet and not in Remove Set', () => {
-        //Arrange
-        const dict = new LWWElementDictionary();
-        const testKey = 'key1';
-        const testData = {
-          string_data: 'a',
-          timestamp: 1,
-        };
-        const testDeleteKey = 'delete1';
+      describe('Case(2), add one data to AddSet, and remove one data with non-exist key', () => {
+        it('should not affect other key in AddSet and not in Remove Set', () => {
+          //Arrange
+          const dict = new LWWElementDictionary();
+          const testKey = 'key1';
+          const testData = {
+            string_data: 'a',
+            timestamp: 1,
+          };
+          const testDeleteKey = 'delete1';
 
-        //Act
-        dict.add(testKey, testData);
-        dict.remove(testDeleteKey, 2);
+          //Act
+          dict.add(testKey, testData);
+          dict.remove(testDeleteKey, 2);
 
-        //Assert
-        expect(dict.getAddSet().size).toBe(1);
-        expect(dict.lookup(testKey)).toBeTruthy();
-        expect(dict.getRemoveSet().size).toBe(0);
-        expect(dict.lookup(testDeleteKey)).toBeFalsy();
+          //Assert
+          expect(dict.getAddSet().size).toBe(1);
+          expect(dict.lookup(testKey)).toBeTruthy();
+          expect(dict.getRemoveSet().size).toBe(0);
+          expect(dict.lookup(testDeleteKey)).toBeFalsy();
+        });
       });
-      it('Case(3), add remove to RemoveSet(T=1), then add same key to AddSet(T=2), lookup should return true', () => {
-        //Arrange
-        const dict = new LWWElementDictionary();
-        const testKey = 'key1';
-        const testData = {
-          string_data: 'a',
-          timestamp: 2,
-        };
+      describe('Case(3), add remove to RemoveSet(T=1), then add same key to AddSet(T=2), ', () => {
+        it('lookup should return true', () => {
+          //Arrange
+          const dict = new LWWElementDictionary();
+          const testKey = 'key1';
+          const testData = {
+            string_data: 'a',
+            timestamp: 2,
+          };
 
-        //Act
-        dict.remove(testKey, 1);
-        dict.add(testKey, testData);
+          //Act
+          dict.remove(testKey, 1);
+          dict.add(testKey, testData);
 
-        //Assert
-        expect(dict.getAddSet().size).toBe(1);
-        expect(dict.lookup(testKey)).toBeTruthy();
-        expect(dict.getRemoveSet().size).toBe(0);
+          //Assert
+          expect(dict.getAddSet().size).toBe(1);
+          expect(dict.lookup(testKey)).toBeTruthy();
+          expect(dict.getRemoveSet().size).toBe(0);
+        });
       });
     });
   });
   describe('Test Lookup Function', () => {
-    describe('Condition 1: if element is in addSet and element is not in removeSet', () => {
-      it('should return true', () => {
-        const dict = new LWWElementDictionary();
-        dict.add('key1', { timestamp: 1 } as Element);
-        expect(dict.lookup('key1')).toBeTruthy();
+    describe('Normal Conditions', () => {
+      describe('Condition 1: if element is in addSet and element is not in removeSet', () => {
+        it('should return true', () => {
+          const dict = new LWWElementDictionary();
+          dict.add('key1', { timestamp: 1 } as Element);
+          expect(dict.lookup('key1')).toBeTruthy();
+        });
       });
-    });
 
-    describe('Condition 2: if element in Both Set, then if AddSet.timestamp is later then RemoveSet.timestamp', () => {
-      it('should return true', () => {
-        const dict = new LWWElementDictionary();
-        dict.add('key1', { timestamp: 2 } as Element);
-        dict.remove('key1', 1);
-        expect(dict.lookup('key1')).toBeTruthy();
+      describe('Condition 2: if element in Both Set, then if AddSet.timestamp is later then RemoveSet.timestamp', () => {
+        it('should return true', () => {
+          const dict = new LWWElementDictionary();
+          dict.add('key1', { timestamp: 2 } as Element);
+          dict.remove('key1', 1);
+          expect(dict.lookup('key1')).toBeTruthy();
+        });
       });
-    });
-    describe('Condition 3: if Element is not in Add Set', () => {
-      it('should return false', () => {
-        const dict = new LWWElementDictionary();
-        expect(dict.lookup('non-exist-key')).toBeFalsy();
+      describe('Condition 3: if Element is not in Add Set', () => {
+        it('should return false', () => {
+          const dict = new LWWElementDictionary();
+          expect(dict.lookup('non-exist-key')).toBeFalsy();
+        });
       });
     });
   });
@@ -356,7 +373,6 @@ describe('LWW Element Dictionary Testing', () => {
         });
         // Lookup return False because the latest cat in AddSet is T=5, RemoveSet is T=6
         expect(master.lookup('cat')).toBeFalsy();
-        console.log(master.getAddSet(), master.getRemoveSet());
 
         /**
          * Result
